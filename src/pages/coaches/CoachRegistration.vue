6e7d1c82e3c5fc3d84f0cb11e958d9020a7a6469
eliasyoung <warlockD@126.com>
2022-09-27 16:41:48 +0800

    CoachForm Component Init & Styling

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,9 +1,25 @@
 <template>
-  <div>REGISTER</div>
+  <div>
+    <section>
+      <BaseCard>
+        <h2>Register as a coach now!</h2>
+        <CoachForm></CoachForm>
+      </BaseCard>
+    </section>
+  </div>
 </template>
 
 <script>
-export default {};
+import CoachForm from "@/components/coaches/CoachForm.vue";
+
+export default {
+  components: {
+    CoachForm,
+  },
+};
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+
+
+</style>
